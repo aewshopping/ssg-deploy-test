0cca260ba928cd2c6593808de3e650207f1a971a
Glitch (pophist-ssg-test) <none>
2023-01-17 21:51:07 +0000

    🎷🚽 include Books All page

--- public/script.js
+++ public/script.js
@@ -97,15 +97,151 @@ function toggle_layer_scroll(id) {
   document.getElementById("btn-layers-article").classList.toggle("color-accent-fill");
 }
 
-
-function toggle_showdetails(btnID, elem) {
+// to reveal a collapsed div, also rotate an arrow on the expander div if this exists
+function toggle_showdetails(id, elem) {
   document.getElementById(elem).classList.toggle("uncollapsed");
+  document.getElementById(id).classList.toggle("arrow-right");
+  document.getElementById(id).classList.toggle("arrow-down"); // only works if you have a unique expander per collapse otherwise gets confused which way to point the arrow!
 }
 
+// to reveal a collapsed list when focus inside
 function toggle_on_showdetails(btnID, elem) {
   document.getElementById(elem).classList.add("uncollapsed");
 }
 
+// allows you to press enter trigger click event on an div with class expander
+const expanders_all = document.querySelectorAll(".expander");
+expanders_all.forEach(expander => {
+  expander.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {expander.click();}
+  })
+});
+
+
+
+//BOOKS FILTER CODE - START ///////////////////////////////////
+
+// add click event function to all filter butons
+const filterbtns_all = document.querySelectorAll(".filter-btn");
+filterbtns_all.forEach(filterbtn => {
+  filterbtn.addEventListener("click", function(btnID) {
+    myFilterAnd(this.id);
+  })  
+});
+
+function myFilterAnd(id) {
+// this is the main filtering function that calls the sub functions
+
+ //select the button element clicked
+ var myfbtn = document.getElementById(id);
+ myfbtn.classList.toggle("btn-color-accent");
+ myfbtn.classList.toggle("btn-color-light");
+ myfbtn.classList.toggle("onState");
+  
+  myDoFilterAnd();
+
+  if (myfbtn.classList.contains("onState")) {
+    myShowFilterLabel(id)
+  } else {
+    document.getElementById(id + "f").remove();
+  }
+}
+
+
+
+function myShowFilterLabel(id) {
+//  creates a button to match the filter that has just been applied, puts the button above the books so you can see which filters are active, then attaches a function to the button to allow you to remove the filter.
+  
+  var xbtn = document.createElement("button");
+  var location = document.getElementById("myFilterGroup");
+  var btnText = document.getElementById(id).innerHTML;
+  
+  xbtn.type = "button";
+  xbtn.id = id + "f"
+  xbtn.innerHTML = btnText;
+  xbtn.classList.add('btn', 'btn-color-bg', 'btn-rounded', 'ft-size-small', 'my-close');
+  location.appendChild(xbtn);
+  
+  // attach the function to the button
+  xbtn.onclick = function () {
+    myFilterAnd(id);
+    xbtn.remove();
+  };
+}
+
+function myDoFilterAnd() {
+// note that each category needs at least two filters for this filter code to work on that category  
+  
+  var x=0;
+  var z=0;
+  var b=0;
+  var catnumber=Number(document.getElementById("catcounter").dataset.counter)-1; // number of filter categories
+
+  var catbuttons = [];
+  var catbuttonsOn =[];
+  var allbooks = document.getElementsByClassName('hbk');
+
+  
+  for (let i=0;i<=catnumber;i++) {
+    catbuttons.push(document.getElementsByClassName('catbtn'+i));
+    catbuttonsOn.push(document.getElementsByClassName('catbtn'+i + ' onState'));
+}
+
+    //cycle through all the books
+  Loopbooks:
+  for (let i=0;i<allbooks.length;i++) {
+    x=0; //set up match counter
+    z=1;
+    
+    //cycle through the button categories
+    Loop_button_categories:
+    for (let q=0;q<catbuttons.length;q++) {
+      
+      if (catbuttonsOn[q].length===0) {
+        // don't apply filter in the case where no filters selected in a category, ie include book
+        x++;            
+      } else {
+     
+        //cycle through all the filter buttons within the category
+        Loop_buttons_in_a_category:
+        for (let p=0;p<catbuttons[q].length;p++) {
+          var btn_id = catbuttons[q][p].id;        
+
+          if (catbuttons[q][p].classList.contains("onState") && allbooks[i].classList.contains(btn_id)) {
+            //if the p.th button of category q is "on" AND the i.th book has a matching id, then include book. Because within a category it is an OR filter we only need to find one match (or more). When match found, exit the loop
+            x++;         
+            break Loop_buttons_in_a_category;
+          }
+        } // loop buttons within a category
+         
+        }
+
+      // check to see we have as many matches as button categories, if we have missed a match break loop      
+      if (x < q + 1) {
+        z=q
+        break Loop_button_categories;
+      }
+      
+    }  // loop button categories
+
+    // if we have a failed category match, don't show the book
+    if (x < z + 1) {
+      allbooks[i].style.display = "none";
+
+      } else {
+        allbooks[i].style.display = "block";
+        b++;
+      } 
+  
+  } //loop books 
+  
+  // show the number of books we have remaining
+  document.getElementById("bookCount").innerText = "Showing " + b + " books"  
+
+//  window.alert(b + " history books")
+}
+
+//BOOKS FILTER CODE - END ///////////////////////////////////
 
 //PROGRESS BAR CODE ////////////////////////////////////////
 //let lastKnownScrollPosition = 0;
@@ -140,6 +276,7 @@ document.addEventListener('scroll', function(e) {
 });
 //END OF PROGRESS BAR CODE ////////////////////////////////////////
 
+
 //SHARING BUTTON CODE USING WEB SHARE API ///////////////////////// 
 const mytitle = document.title;
 const myurl = document.querySelector('link[rel=canonical]') ? document.querySelector('link[rel=canonical]').href : document.location.href;
@@ -168,8 +305,10 @@ let btnShare = document.getElementsByClassName("share-link");
 
 //MODAL FOR BOOK COVERS /////////////////////
 
-const modal = document.querySelector('.modal_bookzoom');
+const modal_bookzoom = document.querySelector('.modal_bookzoom');
 const modalTitle = document.querySelector('.myModalTitle');
+const modalAuthor = document.querySelector('.myModalAuthor');
+const modalPublishdate = document.querySelector('.myModalPublishdate');
 const modalImage = document.querySelector('.myModalImage');
 const modalLink_AmazonUK = document.querySelector('.myModalLink_AmazonUK');
 const modalLink_AmazonUS = document.querySelector('.myModalLink_AmazonUS');
@@ -177,25 +316,30 @@ const modalLink_review = document.querySelector('.myModalLink_review');
 const modal_review_btn = document.querySelector('.myModal_review_btn');
 const modalClose = document.querySelector('.modal_close');
 
-document.querySelectorAll('.open-modal').forEach(item => {
+document.querySelectorAll('.open_modal_bookzoom').forEach(item => {
   item.addEventListener('click', event => {
 
     const myid = document.getElementById(item.id);
     
     modalTitle.innerText = myid.dataset.title;
+    if (myid.dataset.author) {modalAuthor.innerText = "by " + myid.dataset.author;}
+    if (myid.dataset.publishdate) {
+      let myDate = new Date(myid.dataset.publishdate);
+      modalPublishdate.innerText = getMonthName(myDate.getMonth()) + " " + myDate.getFullYear();
+    }
     modalImage.src = myid.dataset.cover_large;
     modalLink_AmazonUK.href = myid.dataset.amazon_uk_link;
     modalLink_AmazonUS.href = myid.dataset.amazon_us_link;
-    modalLink_review.href = myid.dataset.review;
+    if (myid.dataset.review) {modalLink_review.href = myid.dataset.review;
     
     if (myid.dataset.review.length == 0) {
       modal_review_btn.style.display = "none";
     } else {
       modal_review_btn.style.display = "block";
     }
+  }
     
-    
-    modal.showModal();
+    modal_bookzoom.showModal();
     document.activeElement?.blur(); // removes focus from close button
     console.log(item.id);
 
@@ -204,7 +348,29 @@ document.querySelectorAll('.open-modal').forEach(item => {
 
 
 modalClose.addEventListener("click", () => {
-  modal.close();
+  modal_bookzoom.close();
 });
+
+
+const modal_bookfilter = document.querySelector('.modal_bookfilter');
+const modal_bookfilter_close = document.querySelector('.modal_bookfilter_close');
+
+document.querySelectorAll('.open_modal_bookfilter').forEach(item => {
+  item.addEventListener('click', event => {
+    modal_bookfilter.showModal();
+    document.activeElement?.blur(); // removes focus from close button
+ })
+})
+
+modal_bookfilter_close.addEventListener("click", () => {
+  modal_bookfilter.close();
+});
+
+
+function getMonthName(monthNumber) {
+  const date = new Date();
+  date.setMonth(monthNumber - 1);
+  return date.toLocaleString('en-US', { month: 'long' });
+}
 
 //END OF MODAL FOR BOOK COVERS /////////////////////
---
+++ src/_data/book_categories.js
@@ -0,0 +1,39 @@
+// adapted from https://daniel-saunders.com/posts/notes/using-airtable-as-a-jamstack-cms/
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
+// Setting the Airtable secret API key and the Airtable base id 
+// from environment variables
+const airtableToken = process.env.mykey;
+const airtableBaseId = process.env.mybase;
+const airtableTableId = "tblooZV1SAAnWMpQC";
+
+module.exports = async function() {
+
+    // Constructing a query URL (using the Airtable API URL encoder 
+    // here: https://codepen.io/airtable/pen/MeXqOg) and 
+    // adding in our filter and sort parameters
+    let url = `https://api.airtable.com/v0/${airtableBaseId}/${airtableTableId}?sort%5B0%5D%5Bfield%5D=sort_order&sort%5B0%5D%5Bdirection%5D=asc`;
+
+    // The beauty of Eleventy Fetch -- pass in our URL and 
+    // set a few options for caching and authorization
+    const response = await EleventyFetch(url, {
+
+    // Cache the responses for 1 hour; useful so you don't 
+    // constantly ping and exceed the limits on Airtable's API
+    duration: '2s',
+
+    // Set the data response type
+    type: 'json',
+    directory: ".cache",
+
+    // Append the Airtable API authorization key to the query
+    fetchOptions: {
+        headers: {
+            authorization: `Bearer ${airtableToken}`
+            },
+        },
+    });
+
+    let { records } = await response;
+    return records
+};
